c77ef0b9d9121c7c3119069468e0ef16a86d3078
Van Ortega <ortegavan@live.com>
2024-12-01 16:56:47 -0300

    feat: implementa lista dinâmica de tarefas

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+export type Task = {
+    title: string;
+    completed: boolean;
+};
 
 @Component({
     selector: 'app-list',
@@ -8,4 +13,18 @@ import { CommonModule } from '@angular/common';
     templateUrl: './list.component.html',
     styleUrl: './list.component.scss',
 })
-export class ListComponent {}
+export class ListComponent {
+    tasks = signal<Task[]>([
+        { title: 'Task 1', completed: false },
+        { title: 'Task 2', completed: false },
+        { title: 'Task 3', completed: false },
+        { title: 'Task 1', completed: true },
+        { title: 'Task 2', completed: true },
+        { title: 'Task 3', completed: true },
+    ]);
+
+    todoTasks = computed(() => this.tasks().filter((task) => !task.completed));
+    completedTasks = computed(() =>
+        this.tasks().filter((task) => task.completed)
+    );
+}
