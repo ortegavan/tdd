1015adb48e5ed4453a175d8c929faef7cf4ff14c
Vanessa Ortega <ortegavan@live.com>
2025-02-11 00:37:01 -0300

    test: verifica títulos da aplicação e header

--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -1,8 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
-    let component: HeaderComponent;
     let fixture: ComponentFixture<HeaderComponent>;
 
     beforeEach(async () => {
@@ -11,11 +11,11 @@ describe('HeaderComponent', () => {
         }).compileComponents();
 
         fixture = TestBed.createComponent(HeaderComponent);
-        component = fixture.componentInstance;
         fixture.detectChanges();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it('deve renderizar o título corretamente', () => {
+        const elemento = fixture.debugElement.query(By.css('h1')).nativeElement;
+        expect(elemento.textContent.trim()).toBe('Gerenciador de tarefas');
     });
 });
