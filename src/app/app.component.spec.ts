82028a24a99681dcd400de8952dd19b05dfd33b6
Vanessa Ortega <ortegavan@live.com>
2025-02-11 23:19:59 -0300

    test: cria mock do header

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,35 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { By } from '@angular/platform-browser';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FakeHeaderComponent } from '@testing/mocks/fake-header.component';
 
 describe('AppComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [AppComponent, RouterModule.forRoot([])],
         }).compileComponents();
+
+        TestBed.overrideComponent(AppComponent, {
+            remove: {
+                imports: [HeaderComponent],
+            },
+            add: {
+                imports: [FakeHeaderComponent],
+            },
+        });
     });
 
-    it(`should have as title 'tdd'`, () => {
+    it(`deve renderizar o componente header`, () => {
         const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app.title).toEqual('tdd');
+        const header = fixture.debugElement.query(By.css('app-header'));
+        expect(header).toBeTruthy();
+    });
+
+    it(`deve renderizar o router-outlet`, () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const outlet = fixture.debugElement.query(By.css('router-outlet'));
+        expect(outlet).toBeTruthy();
     });
 });
