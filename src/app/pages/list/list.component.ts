2ca1c02b6b1776402df2368a410db4f703beff90
Vanessa Ortega <ortegavan@live.com>
2025-02-12 19:14:50 -0300

    style: altera design da lista de tarefas

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+interface Task {
+    title: string;
+    completed: boolean;
+}
 
 @Component({
     selector: 'app-list',
@@ -7,4 +12,18 @@ import { CommonModule } from '@angular/common';
     templateUrl: './list.component.html',
     styleUrl: './list.component.scss',
 })
-export class ListComponent {}
+export class ListComponent {
+    tasks = signal<Task[]>([
+        { title: 'Task 1', completed: false },
+        { title: 'Task 2', completed: false },
+        { title: 'Task 3', completed: false },
+        { title: 'Task 4', completed: true },
+        { title: 'Task 5', completed: true },
+        { title: 'Task 6', completed: true },
+    ]);
+
+    todoTasks = computed(() => this.tasks().filter((task) => !task.completed));
+    completedTasks = computed(() =>
+        this.tasks().filter((task) => task.completed)
+    );
+}
